5a319c1647f58a24ceb620cc2fda6191e32a1906
Abul Hasnat Nayeem <sakil58@gmail.com>
2018-04-02 00:14:22 +0600

    Set global onclick listener in new items fragment

--- app/src/main/java/com/cardview/FragmentNewItems.java
+++ app/src/main/java/com/cardview/FragmentNewItems.java
@@ -1,21 +1,30 @@
 package com.cardview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
 
 public class FragmentNewItems extends Fragment {
 
     View v;
-    Button b;
+    private TableLayout mainTableLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         v = inflater.inflate(R.layout.fragment_new_items_layout, container, false);
+        mainTableLayout = (TableLayout) v.findViewById(R.id.category_table_layout);
+        mainTableLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), DrugDetails.class));
+            }
+        });
         return v;
     }
 }
